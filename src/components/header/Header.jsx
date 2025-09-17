89d58fa2da496148eec1470bca9abf2ac984ac97
Ulug'bek Abdusamatov <ulugbek2011not@gmail.com>
2025-09-17 16:35:06 +0500

    meniki

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import  './header.css'
+
+function Header() {
+  return (
+    <div>
+        <nav className='container'>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/brands">Brands</Link></li>
+                <li><Link to="/contacts">Contacts</Link></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import Header from '../../components/header/Header'
+import  './contact.css'
+
+function Contact() {
+  return (
+    <div>
+        <Header></Header>
+        <main>
+            
+        </main>
+    </div>
+  )
+}
+
+export default Contact
